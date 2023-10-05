7cd69f0809ad3d57835b653c7a770dff97bd3155
gaalbesa <galbesa@alumnat.copernic.cat>
2023-10-05 16:31:47 +0200

    put the JAVADOC of all files

--- chat/src/main/java/com/projects/App.java
+++ chat/src/main/java/com/projects/App.java
@@ -1,7 +1,19 @@
 package com.projects;
 
 import java.io.IOException;
+
+import com.projects.functions.ConexionServer;
+
+/**
+ * The `App` class serves as the entry point for your server application. When
+ * executed, it starts the `ConexionServer` to listen for incoming client
+ * connections.
+ */
 public class App {
+    /**
+     * The main method of the server application. It starts the `ConexionServer` to
+     * listen for incoming client connections.
+     */
     public static void main(String[] args) throws IOException {
         ConexionServer.startServer();
     }
