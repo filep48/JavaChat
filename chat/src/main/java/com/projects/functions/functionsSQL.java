a08dee11ae7f8047d34d4d835f3b2f3a864dd4f2
Gerard Albesa Trujillo <galbesa@alumnat.copernic.cat>
2023-10-09 17:36:16 +0000

    Update file functionsSQL.java

--- chat/src/main/java/com/projects/functions/functionsSQL.java
+++ chat/src/main/java/com/projects/functions/functionsSQL.java
@@ -6,9 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.swing.JOptionPane;
-
 import com.projects.clases.Usuario;
 import com.projects.functions.FuncionesServer;
 
