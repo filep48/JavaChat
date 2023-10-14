626c24a5c3788498345541e6876588b5582913ad
Vpedrola <vpedrola@alumnat.copernic.cat>
2023-10-14 16:42:36 +0200

    He quitado todos los JOptionpane

--- server/src/main/java/srv/proyecto/functions/functionsSQL.java
+++ server/src/main/java/srv/proyecto/functions/functionsSQL.java
@@ -1,13 +1,17 @@
 package srv.proyecto.functions;
 
+import java.io.Console;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.swing.JOptionPane;
+
 import srv.proyecto.clases.Usuario;
 
 public class functionsSQL {
@@ -74,67 +78,46 @@ public class functionsSQL {
             return null;
         }
     }
-
-    /**
-     * Función que recoge datos por teclado del usuario y la envia a función q
-     * valida la regex,
-     */
-    public static Usuario datosUsuario() {
-        while (true) {
-            String nombreUsuario = JOptionPane.showInputDialog(null, "Introduce tu nombre de usuario");
-            String contrasenaUsuario = JOptionPane.showInputDialog(null, "Introduce tu contraseña");
-
-            if (nombreUsuario != null && !nombreUsuario.isEmpty() && contrasenaUsuario != null
-                    && !contrasenaUsuario.isEmpty()) {
-                try {
-                    FuncionesServer.validarContrasena(contrasenaUsuario);
-                    return new Usuario(nombreUsuario, contrasenaUsuario);
-                } catch (FuncionesServer.ContrasenaInvalidaException ex) {
-                    JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage());
-                }
-            } else {
-                JOptionPane.showMessageDialog(null, "Por favor, ingresa un nombre de usuario y contraseña válidos.");
-            }
-        }
-    }
-
-    /** Función que comprueba en bbbdd si existe o no y devuelve un booleano */
-    public static void consultaBbddUsuarioExiste(Usuario datos, Connection cn) {
+    public static Usuario datosUsuario(Connection cn, String nombre, String contrasena) {
         try {
-            String strSql = "SELECT nombre_usuario, contrasena FROM usuarios";
+            String strSql = "SELECT nombre_usuario FROM Usuarios WHERE nombre_usuario = ? AND contrasena = ?";
             PreparedStatement pst = cn.prepareStatement(strSql);
-
+            pst.setString(1, nombre);
+            pst.setString(2, contrasena);
+    
             ResultSet rs = pst.executeQuery();
-            boolean usuarioExiste = false;
-            while (rs.next() && !usuarioExiste) {
-                System.out.println(rs.getString("nombre_usuario") + " " + rs.getString("contrasena"));
-                if (rs.getString("nombre_usuario").equals(datos.getNombreUsuarioo())
-                        && rs.getString("contrasena").equals(datos.getContrasena())) {
-                    System.out.println("Usuario existe");
-                    usuarioExiste = true;
-                }
+            if (rs.next()) {
+                System.out.println("Sesión iniciada con éxito.");
+                return new Usuario(nombre, contrasena);
+            } else {
+                System.out.println("Error al iniciar sesión. Usuario o contraseña incorrectos.");
+                return null;
             }
-            if (usuarioExiste == false) {
-                System.out.println("Usuario o contraseña no existen");
-            }
-
         } catch (SQLException ex) {
-            Logger.getLogger(FuncionesServer.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Error al acceder a la base de datos: " + ex.getMessage());
+            return null;
         }
     }
+    
 
     // *********************************************
     public static void creacionGruposBBDD(Connection cn) {
-        try {
-            String strSql = "insert into grupos (nombre_grupo) values (?)";
-            PreparedStatement pst = cn.prepareStatement(strSql);
-            String nombreGrupo = JOptionPane.showInputDialog(null, "Introduce el nombre del grupo");
-            pst.setString(1, nombreGrupo);
-            pst.executeUpdate();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    try {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce el nombre del grupo: ");
+        String nombreGrupo = scanner.nextLine();
+        
+        String strSql = "INSERT INTO grupos (nombre_grupo) VALUES (?)";
+        PreparedStatement pst = cn.prepareStatement(strSql);
+        pst.setString(1, nombreGrupo);
+        pst.executeUpdate();
+        
+        System.out.println("Grupo creado con éxito.");
+    } catch (Exception e) {
+        e.printStackTrace();
     }
+}
+
 
     public static void EliminacionGruposBBDD(Connection cn) {
         try {
@@ -148,61 +131,16 @@ public class functionsSQL {
         }
     }
 
-    // TODAS MIS FUNCIONES AQUI
-    /**
-     * Función que recoge datos por teclado del usuario y la envia a función q
-     * valida la regex,
-     */
-
-    // ----------------cambios---------
-    // 1. se añade una comprobacion de si el usuario existe o no para manejar una
-    // excepcion de sql, ya q el nombre es PKunique.
-    public static Usuario datosUsuario(Connection cn) {
-        while (true) {
-            String nombreUsuario = JOptionPane.showInputDialog(null, "Introduce tu nombre de usuario");
-            String contrasenaUsuario = JOptionPane.showInputDialog(null, "Introduce tu contraseña");
-
-            if (nombreUsuario != null && !nombreUsuario.isEmpty() && contrasenaUsuario != null
-                    && !contrasenaUsuario.isEmpty()) {
-                try {
-                    // Verificar si el nombre de usuario ya existe en la base de datos
-                    if (usuarioExiste(new Usuario(nombreUsuario, contrasenaUsuario), cn)) {
-                        JOptionPane.showMessageDialog(null,
-                                "El nombre de usuario ya está en uso. Por favor, elige otro.");
-                    } else {
-                        // El nombre de usuario no existe
-                        FuncionesServer.validarContrasena(contrasenaUsuario);
-                        return new Usuario(nombreUsuario, contrasenaUsuario);
-                    }
-                } catch (FuncionesServer.ContrasenaInvalidaException ex) {
-                    JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage());
-                }
-            } else {
-                try {
-                    int respuesta = JOptionPane.showConfirmDialog(null, "¿Deseas salir?", "Salir",
-                            JOptionPane.YES_NO_OPTION);
-                    if (respuesta == JOptionPane.YES_OPTION) {
-                        // Salir
-                        return null;
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
     /**
      * Función que comprueba en bbbdd si existe o no y devuelve un booleano.
      * si no existe, llama a la función darAltaUsuario.
      */
 
-    public static boolean usuarioExiste(Usuario datos, Connection cn) {
+    public static boolean usuarioExiste(Connection cn, Usuario datos) {
         try {
-            String strSql = "SELECT nombre_usuario, contrasena FROM usuarios WHERE nombre_usuario = ? AND contrasena = ?";
+            String strSql = "SELECT nombre_usuario FROM Usuarios WHERE nombre_usuario = ?";
             PreparedStatement pst = cn.prepareStatement(strSql);
             pst.setString(1, datos.getNombreUsuarioo());
-            pst.setString(2, datos.getContrasena());
 
             ResultSet rs = pst.executeQuery();
             return rs.next(); // Si hay resultados: el usuario existe en la base de datos
@@ -217,45 +155,43 @@ public class functionsSQL {
      * devuelve true o false
      */
 
-    public static void login(Usuario datos, Connection cn) {
-        if (usuarioExiste(datos, cn)) {
-            System.out.println("LLAMAR AQUI A MENU PARA ENTRAR AL PROGRAMA");
-            JOptionPane.showMessageDialog(null, "Usuario existe");
+    public static void login(Connection cn, Usuario datos) {
+        if (usuarioExiste(cn, datos)) {
+            System.out.println("Usuario existe LLAMAR AL MENU 2.");
+            // Llamar aquí a la función de menú para entrar al programa.
         } else {
-            JOptionPane.showMessageDialog(null, "Usuario o contraseña no existen");
-            darAltaUsuario(datos, cn);
+            System.out.println("Usuario o contraseña no existen.");
+            darAltaUsuario(cn, datos);
         }
     }
 
     // *********************************************
 
     /** función que da de alta a un usuario en la bbdd */
-    public static boolean darAltaUsuario(Usuario usuarioDatos, Connection cn) {
-        if (JOptionPane.showConfirmDialog(null, "Quieres dar de alta el Usuario?") == JOptionPane.YES_OPTION) {
+    public static void darAltaUsuario(Connection cn, Usuario usuarioDatos) {
+        Console console = System.console();
+        if (console != null) {
+            char[] contrasenaChars = console.readPassword("Introduce tu contraseña: ");
+            String contrasena = new String(contrasenaChars);
             try {
-
-                String strSql = "INSERT INTO usuarios (nombre_usuario, contrasena) VALUES (?, ?)";
+                String strSql = "INSERT INTO Usuarios (nombre_usuario, contrasena) VALUES (?, ?)";
                 PreparedStatement pst = cn.prepareStatement(strSql);
                 pst.setString(1, usuarioDatos.getNombreUsuarioo());
-                pst.setString(2, usuarioDatos.getContrasena());
+                pst.setString(2, contrasena);
 
-                // Ejecutar la sentencia SQL
                 int filasAfectadas = pst.executeUpdate();
 
-                // Verificar si se insertó
                 if (filasAfectadas == 1) {
-                    System.out.println("LLAMAR AQUI A MENU PARA ENTRAR AL PROGRAMA");
-                    return true;
+                    System.out.println("Usuario dado de alta con éxito.");
+                } else {
+                    System.out.println("Error al dar de alta al usuario.");
                 }
-                return false;
             } catch (SQLException ex) {
-
-                ex.printStackTrace();
-                return false;
+                Logger.getLogger(FuncionesServer.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.println("Error al dar de alta al usuario.");
             }
         } else {
-            JOptionPane.showMessageDialog(null, "No se ha dado de alta el usuario");
-            return false;
+            System.out.println("La consola no está disponible. No se puede ingresar la contraseña.");
         }
     }
 
